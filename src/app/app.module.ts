f4c0ca96d9beb0751d54124fb03dc84326cbbfee
ellaouzi <mohamed.ellaouzi@gmail.com>
2019-01-12 21:16:49 +0100

    Verified version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Toast } from '@ionic-native/toast';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DataServiceProvider } from '../providers/data-service/data-service';
+ 
+import {HttpClientModule} from '@angular/common/http'; 
+import { HttpModule } from '@angular/http'
 
 @NgModule({
   declarations: [
@@ -14,7 +19,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +31,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BarcodeScanner,
+    Toast,
+    DataServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,85 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner , BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
+import { Toast } from '@ionic-native/toast';
+import { DataServiceProvider } from '../../providers/data-service/data-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+options: BarcodeScannerOptions;
+encodText:string="";
+encodedData:any={};
+scannedData:any={};
 
-  constructor(public navCtrl: NavController) {
+  products: any[] = [];
+  selectedProduct: any;
+  productFound:boolean = false;
+  test= "test";
 
+
+  constructor(public navCtrl: NavController,
+    public scanner: BarcodeScanner,
+    private barcodeScanner: BarcodeScanner,
+    private toast: Toast,
+    public dataService: DataServiceProvider) {
+      this.dataService.getProducts()
+        .subscribe((response)=> {
+            this.products = response
+            console.log(this.products);
+        });
+  }
+
+  scan() {
+    this.test="ELLAOUZI Mohamed";
+    console.log('OKKKKK');
+    
+    this.selectedProduct = {};
+    this.barcodeScanner.scan().then((barcodeData) => {
+      this.selectedProduct = this.products.find(product => product.plu === barcodeData.text);
+      if(this.selectedProduct !== undefined) {
+        this.productFound = true;
+        console.log(this.selectedProduct);
+      } else {
+        this.selectedProduct = {};
+        this.productFound = false;
+        this.toast.show('Product not found', '5000', 'center').subscribe(
+          toast => {
+            console.log(toast);
+          }
+        );
+      }
+    }, (err) => {
+      this.toast.show(err, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
+  }
+
+
+
+  scan2(){
+    this.options ={
+      prompt: 'Scan your barcode'
+    };
+    this.scanner.scan(this.options).then((data) => {
+      this.scannedData=data;
+    } , (err) => {
+      console.log('Error', err);
+    })
+  }
+  encode(){
+    this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodText).then((data)=>{
+      this.encodedData=data;
+    },
+     (err) => {
+      console.log('Error', err);
+
+     })
   }
 
 }
