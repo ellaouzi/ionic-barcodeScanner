48396b9429ea968b49108829b80b70b4ba5d69c8
ellaouzi <mohamed.ellaouzi@gmail.com>
2019-01-30 15:45:40 +0100

    Prestation Mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,26 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { Toast } from '@ionic-native/toast';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DetailPage } from '../pages/detail/detail';
+import { AdherentsPage } from '../pages/adherents/adherents';
+import { AgentPage } from '../pages/agent/agent';
+import { PrestationSuccessPage } from '../pages/prestation-success/prestation-success';
+import { PrestationPage } from '../pages/prestation/prestation';
 import { DataServiceProvider } from '../providers/data-service/data-service';
- 
-import {HttpClientModule} from '@angular/common/http'; 
+
+import {HttpClientModule} from '@angular/common/http';
 import { HttpModule } from '@angular/http'
+import { PeopleServiceProvider } from '../providers/people-service/people-service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailPage,
+    AdherentsPage,
+    AgentPage,
+    PrestationPage,
+    PrestationSuccessPage
   ],
   imports: [
     BrowserModule,
@@ -26,7 +37,12 @@ import { HttpModule } from '@angular/http'
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailPage,
+    AdherentsPage,
+    AgentPage,
+    PrestationPage,
+    PrestationSuccessPage
   ],
   providers: [
     StatusBar,
@@ -34,7 +50,8 @@ import { HttpModule } from '@angular/http'
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     BarcodeScanner,
     Toast,
-    DataServiceProvider
+    DataServiceProvider,
+    PeopleServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,46 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { BarcodeScanner , BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
-import { Toast } from '@ionic-native/toast';
-import { DataServiceProvider } from '../../providers/data-service/data-service';
+import {
+  Component
+} from '@angular/core';
+import {
+  NavController
+} from 'ionic-angular';
+import {
+  BarcodeScanner,
+  BarcodeScannerOptions
+} from '@ionic-native/barcode-scanner';
+import {
+  Toast
+} from '@ionic-native/toast';
+import {
+  DataServiceProvider
+} from '../../providers/data-service/data-service';
+import {
+  DetailPage
+} from '../../pages/detail/detail'
+import { AdherentsPage } from '../adherents/adherents';
+import { AgentPage } from '../agent/agent';
+import { PrestationPage } from '../prestation/prestation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-options: BarcodeScannerOptions;
-encodText:string="";
-encodedData:any={};
-scannedData:any={};
 
+  private detailPage;
+  private adherentsPage;
+  private agentPage;
+  private prestationPage;
+  options: BarcodeScannerOptions;
+  encodText: string = "";
+  encodedData: any = {};
+  scannedData: any = {};
   products: any[] = [];
+  pprScanned:String="[ PPR : 6637373][Ajd,jjjd]";
+
   selectedProduct: any;
-  productFound:boolean = false;
-  test= "test";
+  productFound: boolean = false;
+  test = "test";
 
 
   constructor(public navCtrl: NavController,
@@ -25,23 +48,35 @@ scannedData:any={};
     private barcodeScanner: BarcodeScanner,
     private toast: Toast,
     public dataService: DataServiceProvider) {
-      this.dataService.getProducts()
-        .subscribe((response)=> {
-            this.products = response
-            console.log(this.products);
-        });
+     // var txt = "#div-name-1234-characteristic:561613213213";
+     // var numb = this.pprScanned.match(/\d/g);
+     //// this.pprScanned = numb.join("");
+     // alert (numb);​
+    //  this.pprScanned= this.pprScanned;//.replace(/[^0-9]/g,'');
+      this.detailPage = DetailPage;
+      this.adherentsPage = AdherentsPage;
+      this.agentPage = AgentPage;
+      this.prestationPage = PrestationPage;
+
+    this.dataService.getProducts()
+      .subscribe((response) => {
+        this.products = response
+        console.log(this.products);
+      });
   }
 
-  scan() {
-    this.test="ELLAOUZI Mohamed";
-    console.log('OKKKKK');
-    
-    this.selectedProduct = {};
-    this.barcodeScanner.scan().then((barcodeData) => {
-      this.selectedProduct = this.products.find(product => product.plu === barcodeData.text);
-      if(this.selectedProduct !== undefined) {
-        this.productFound = true;
-        console.log(this.selectedProduct);
+
+
+  scan2() {
+    this.options = {
+      prompt: 'Scanner votre carte adhérent'
+    };
+    this.scanner.scan(this.options).then((data) => {
+      this.scannedData = data;
+      //===========================/ME
+      if (this.scannedData !== undefined) {
+
+          this.loadDetail(data);
       } else {
         this.selectedProduct = {};
         this.productFound = false;
@@ -51,35 +86,99 @@ scannedData:any={};
           }
         );
       }
+      //===========================/ME
+
     }, (err) => {
-      this.toast.show(err, '5000', 'center').subscribe(
-        toast => {
-          console.log(toast);
-        }
-      );
-    });
-  }
-
-
-
-  scan2(){
-    this.options ={
-      prompt: 'Scan your barcode'
-    };
-    this.scanner.scan(this.options).then((data) => {
-      this.scannedData=data;
-    } , (err) => {
       console.log('Error', err);
     })
   }
-  encode(){
-    this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodText).then((data)=>{
-      this.encodedData=data;
-    },
-     (err) => {
-      console.log('Error', err);
 
-     })
+
+  encode() {
+    this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodText).then((data) => {
+        this.encodedData = data;
+      },
+      (err) => {
+        console.log('Error', err);
+
+      })
   }
 
+  loadDetail(ppr) {
+    this.navCtrl.push(this.detailPage,{ppr:ppr});
+  }
+
+  adherentAPI() {
+    this.navCtrl.push(this.adherentsPage);
+  }
+
+
+  loadAgentPage(ppr) {
+    this.navCtrl.push(this.agentPage,{ppr:ppr});
+  }
+
+  loadPrestationPage(ppr) {
+    this.navCtrl.push(this.prestationPage,{ppr:ppr});
+  }
+testPrestationPage() {
+    this.navCtrl.push(this.prestationPage);
+  }
+
+
+  addPrestation() {
+    console.log('addPrestation');
+    this.navCtrl.push(this.prestationPage);
+  }
+
+  scan1() {
+    this.options = {
+      prompt: 'Scanner votre carte adhérent'
+    };
+    this.scanner.scan(this.options).then((data) => {
+      this.scannedData = data;
+      //===========================/ME
+      if (this.scannedData !== undefined) {
+
+        this.loadPrestationPage(data);
+      } else {
+        this.selectedProduct = {};
+        this.productFound = false;
+        this.toast.show('Product not found', '5000', 'center').subscribe(
+          toast => {
+            console.log(toast);
+          }
+        );
+      }
+      //===========================/ME
+
+    }, (err) => {
+      console.log('Error', err);
+    })
+  }
+
+  scan3() {
+    this.options = {
+      prompt: 'Scanner votre carte adhérent'
+    };
+    this.scanner.scan(this.options).then((data) => {
+      this.scannedData = data;
+      //===========================/ME
+      if (this.scannedData !== undefined) {
+
+          this.loadAgentPage(data);
+      } else {
+        this.selectedProduct = {};
+        this.productFound = false;
+        this.toast.show('Product not found', '5000', 'center').subscribe(
+          toast => {
+            console.log(toast);
+          }
+        );
+      }
+      //===========================/ME
+
+    }, (err) => {
+      console.log('Error', err);
+    })
+  }
 }
